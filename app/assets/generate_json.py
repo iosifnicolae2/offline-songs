12ad8910a095bc21ee501a2ce86fb8c8f6cc4a30
Nicolae Iosif <iosifnicolae2@gmail.com>
2017-12-10 21:28:05 +0200

    fix bugs

---
+++ app/assets/generate_json.py
@@ -0,0 +1,39 @@
+import os
+import json
+import chardet
+
+ALBUMS_DIR = 'albums'
+SONGS_JSON_FILE = 'songs.json'
+
+songs_json = {}
+song_id = 1;
+albums = os.listdir(ALBUMS_DIR)
+
+for album in albums:
+    if 'DS_Store' in album:
+        continue
+
+    album_dir = '%s/%s' % (ALBUMS_DIR, album)
+    songs = os.listdir(album_dir)
+    for song in songs:
+        if 'DS_Store' in song:
+            continue
+
+        song_file = "%s/%s" %(album_dir, song)
+        with open(song_file, 'rb') as file:
+            # print(song_file)
+            rawdata = b''.join(file.readlines())
+            song_text = chardet.detect(rawdata)['encoding']
+        songs_json[song_id] = {
+    		"id": song_id,
+            "name": song.replace('.txt', ''),
+    		"category": album,
+    		"number": "",
+    		"text": rawdata.decode(song_text)
+    	}
+        print(song_id)
+        song_id =  song_id + 1
+
+
+with open(SONGS_JSON_FILE, 'w') as outfile:
+    json.dump(songs_json, outfile)
